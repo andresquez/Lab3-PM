e392b0b392a5a46ebc744b5eaf2be64fd052b73c
Andres Eduardo Quezada Bendaña <andresequez@gmail.com>
2022-07-23 14:46:59 -0600

    Removed Unnecessary function

--- lab3.kt
+++ lab3.kt
@@ -18,29 +18,20 @@ fun main() {
     println(processList(listOf(25, "2", null, false,null,true,"hola")))
 }
 
-fun infoOf(obj : Any?):String? {//function that gets the info of the item in list
-    return when (obj) {
-        is Int -> return when{
-        num % 10 == 0 ->  "M10"
-        num % 5 == 0 -> "M5"
-        num % 2 == 0 -> "M2"
-        else -> null}
-        is String -> "L${obj.length}"
-        true -> "Verdadero"
-        false -> "Falso"
-        else -> null
-      }
-}
-
 fun processList(inputList: List<Any?>?): List<ItemData>? {
     if (inputList == null) return null
     val wList = ArrayList<ItemData>()
     for (item in inputList.orEmpty()){
        item?.let{
             when (item){
-                is Int -> wList.add(ItemData(inputList.indexOf(item),item,"entero",infoOf(item)))
-                is String -> wList.add(ItemData(inputList.indexOf(item),item,"cadena",infoOf(item)))
-                is Boolean -> wList.add(ItemData(inputList.indexOf(item),item,"booleano",infoOf(item)))
+                is Int -> wList.add(ItemData(inputList.indexOf(item),item,"entero",when{
+                    item % 10 == 0 ->  "M10"
+                    item % 5 == 0 -> "M5"
+                    item % 2 == 0 -> "M2"
+                    else -> null}))
+                is String -> wList.add(ItemData(inputList.indexOf(item),item,"cadena","L${item.length}"))
+                true -> wList.add(ItemData(inputList.indexOf(item),item,"booleano","verdadero"))
+                false -> wList.add(ItemData(inputList.indexOf(item),item,"booleano","falso"))
                 else -> wList.add(ItemData(inputList.indexOf(item),item,null,null))
             }
         }
